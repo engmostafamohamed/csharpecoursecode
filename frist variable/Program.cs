9fdba72b96557c76a17b088efd7e0bcbcae2e65b
engmostafamohamed <engmostafamohamed@github.com>
2017-08-21 14:41:56 +0300

    frist csharpe program
     this is frist csharpe program

---
+++ frist variable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frist_variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 7;
+            Console.WriteLine("my variable = {0}", x);
+            Console.ReadLine();
+        }
+    }
+}
